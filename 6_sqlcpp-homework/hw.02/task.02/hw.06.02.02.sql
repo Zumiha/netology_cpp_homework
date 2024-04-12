8ef0e3e9ea0a0469b9aa739637a57b501f4d4e2b
Zub Mik <zubetzmikhael@gmail.com>
2024-04-12 15:58:28 +0300

    added corrections to task.02.02

--- 6_sqlcpp-homework/hw.02/task.02/hw.06.02.02.sql
+++ 6_sqlcpp-homework/hw.02/task.02/hw.06.02.02.sql
@@ -3,15 +3,10 @@ CREATE TABLE IF NOT EXISTS department (
 	department_name varchar(60) NOT null
 );
 
-create table if not exists dephead (
-	id serial primary key,
-	department_id int references department(id)	
-);
-
 CREATE TABLE IF NOT EXISTS employee (
 	id serial PRIMARY KEY ,
 	employee_name varchar(60) NOT null,
 	department_id int references department(id),
-	dephead_id int references dephead(id)
+	dephead_id int references employee(id)
 );
 
