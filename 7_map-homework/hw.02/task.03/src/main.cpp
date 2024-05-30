2fa44472d8b28d98d7d279c02262a15c6065379d
Zub Mik <zubetzmikhael@gmail.com>
2024-05-30 18:45:19 +0300

    fixes to 07_hw.02.03 solution

--- 7_map-homework/hw.02/task.03/src/main.cpp
+++ 7_map-homework/hw.02/task.03/src/main.cpp
@@ -19,25 +19,29 @@ public:
 
 void swapLock(Data& _a, Data& _b) {
     std::lock (_a.mutex, _b.mutex);
-    int temp = _a.getVal();
-    _a.setVal(_b.getVal());
-    _b.setVal(temp);
-    _a.mutex.unlock(); _b.mutex.unlock();
-}
-
-void sawpScopedLock (Data& _a, Data& _b) {
-    std::scoped_lock scope_lock (_a.mutex, _b.mutex);
+    // std::unique_lock unq_lock1 (_a.mutex, std::adopt_lock);
+    // std::unique_lock unq_lock2 (_b.mutex, std::adopt_lock);
+    std::lock_guard lck_guard1 (_a.mutex, std::adopt_lock);
+    std::lock_guard lck_guard2 (_b.mutex, std::adopt_lock);
     int temp = _a.getVal();
     _a.setVal(_b.getVal());
     _b.setVal(temp);
 }
 
-void sawpUniqueLock (Data& _a, Data& _b) {
-    std::unique_lock unq_lock1 (_a.mutex);
-    std::unique_lock unq_lock2 (_b.mutex);
+void swapScopedLock (Data& _a, Data& _b) {
+    std::scoped_lock scpd_lock (_a.mutex, _b.mutex);
     int temp = _a.getVal();
     _a.setVal(_b.getVal());
-    _b.setVal(temp);    
+    _b.setVal(temp);
+}
+
+void swapUniqueLock (Data& _a, Data& _b) {
+    std::unique_lock unq_lock1 (_a.mutex, std::defer_lock);
+    std::unique_lock unq_lock2 (_b.mutex, std::defer_lock);
+    std::lock (unq_lock1, unq_lock2);
+    int temp = _a.getVal();
+    _a.setVal(_b.getVal());
+    _b.setVal(temp);
 }
 
 enum TYPE {SL,SSL,SUL};
@@ -52,15 +56,15 @@ int main() {
     std::cout << std::endl;
 
     a_data.setVal(31), b_data.setVal(49);
-    std::cout << "Before sawpScopedLock: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
-    sawpScopedLock(a_data, b_data);
-    std::cout << "After sawpScopedLock: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
+    std::cout << "Before swapScopedLock: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
+    swapScopedLock(a_data, b_data);
+    std::cout << "After swapScopedLock: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
     std::cout << std::endl;
 
     a_data.setVal(73), b_data.setVal(97);
-    std::cout << "Before sawpUniqueLock: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
-    sawpUniqueLock(a_data, b_data);
-    std::cout << "After sawpUniqueLock: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
+    std::cout << "Before swapUniqueLock: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
+    swapUniqueLock(a_data, b_data);
+    std::cout << "After swapUniqueLock: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
     std::cout << std::endl;
 
 
@@ -70,12 +74,12 @@ int main() {
     // std::cout << "Before swapping: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
     // std::cout << std::endl;
     // std::vector<std::thread> thrd_v;
-    // thrd_v.push_back(std::thread(swapper, std::ref(a_data), std::ref(b_data), SL, 300));
-    // std::cout << "After thread1: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
-    // thrd_v.push_back(std::thread(swapper, std::ref(a_data), std::ref(b_data), SSL, 200));
-    // std::cout << "After thread2: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
-    // thrd_v.push_back(std::thread(swapper, std::ref(a_data), std::ref(b_data), SUL, 100));
+    // thrd_v.push_back(std::thread(swapper, std::ref(a_data), std::ref(b_data), SUL, 500));
     // std::cout << "After thread3: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
+    // thrd_v.push_back(std::thread(swapper, std::ref(a_data), std::ref(b_data), SL, 500));
+    // std::cout << "After thread1: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
+    // thrd_v.push_back(std::thread(swapper, std::ref(a_data), std::ref(b_data), SSL, 500));
+    // std::cout << "After thread2: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << std::endl;
     // std::cout << std::endl;
     // for (auto& i : thrd_v) {i.join();}
     // std::cout << "After swapping: a_data = " << a_data.getVal() << ", b_data = " << b_data.getVal() << "\n" << std::endl;
@@ -92,12 +96,12 @@ void swapper (Data& _a, Data& _b, TYPE code, int timer) {
 
     case SSL:
         std::this_thread::sleep_for(std::chrono::milliseconds(timer));
-        sawpScopedLock(_a, _b);
+        swapScopedLock(_a, _b);
         break;
 
     case SUL:
         std::this_thread::sleep_for(std::chrono::milliseconds(timer));
-        sawpUniqueLock(_a, _b);
+        swapUniqueLock(_a, _b);
         break;
     
     default:
