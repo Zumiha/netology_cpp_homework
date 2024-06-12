24d76162e8453ddc9100081f6698cf30315a6a92
ZuMi <zubetzmikhael@gmail.com>
2024-06-12 19:57:10 +0300

    performed refactoring attempt for 07_hw04.01 task

--- 7_map-homework/hw.04/shape.cpp
+++ 7_map-homework/hw.04/shape.cpp
@@ -1,116 +1,57 @@
 #define _USE_MATH_DEFINES
 #include"shape.h"
 #include<cmath>
-Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8)
-{
-	type = _type;
-	// заполн¤ем координаты фигуры
-	switch (type)
-	{
-	case line: 
-		x1 = _x1; y1 = _y1;
-		x2 = _x2; y2 = _y2;
-		break;
-	case sqr:
-		x1 = _x1; y1 = _y1;
-		x2 = _x2; y2 = _y2;
-		x3 = _x3; y3 = _y3;
-		x4 = _x4; y4 = _y4;
-		break;
-	case cube:
-		x1 = _x1; y1 = _y1; z1 = _z1;
-		x2 = _x2; y2 = _y2; z2 = _z2;
-		x3 = _x3; y3 = _y3; z3 = _z3;
-		x4 = _x4; y4 = _y4; z4 = _z4;
-		x5 = _x5; y5 = _y5; z5 = _z5;
-		x6 = _x6; y6 = _y6; z6 = _z6; 
-		x7 = _x7; y7 = _y7; z7 = _z7;
-		x8 = _x8; y8 = _y8; z8 = _z8;
-		break;
-	default:
-		break;
-	}
-	
-	// стороны фигуры
-	int a = abs(x1 - x2);
-	int b = abs(y1 - y2);
-	int c = abs(z1 - z2);
-	// считаем площадь фигуры
-	switch (type)
-	{
-	case line:
-		square = 0;
-		break;
-	case sqr:
-		square = a * b;
-		break;
-	case cube:
-		square = 2 * a * b + 2 * a * c + 2 * b * c;
-		break;
-	default:
-		break;
-	}
 
-	// считаем объем фигуры
-	switch (type)
-	{
-	case line:
-		volume = 0;
-		break;
-	case sqr:
-		volume = 0;
-		break;
-	case cube:
-		volume = a * b * c;
-		break;
-	default:
-		break;
-	}
+Shape::Shape(Type extType) : type(extType) {
+	switch (extType) {
+	case LINE: this->shape_name = "Line"; break;
+	case SQUARE: this->shape_name = "Rectangle"; break;
+	case CIRLCE: this->shape_name = "Circle"; break;
+	case CUBE: this->shape_name = "Parallelepiped"; break;
+	case CYLINDER: this->shape_name = "Cylinder"; break;
+	default: this->shape_name = "Figure"; break;
+	}		
+}
+Shape::~Shape() {}
 
+double Shape::calcLength(pointCoord point_one, pointCoord point_two) const {
+	return std::sqrt(
+		std::pow(point_one.p_x - point_two.p_x, 2) + 
+		std::pow(point_one.p_y - point_two.p_y, 2) + 
+		std::pow(point_one.p_z - point_two.p_z, 2) );
 }
 
-Shape::Shape(int type, int _x1, int _y1, double R, double H)
-{
-	// заполн¤ем координаты фигуры
-	switch (type)
-	{
-	case circle:
-		x1 = _x1; y1 = _y1;
-		radius = R;
-		break;
-	case cylinder:
-		x1 = _x1; y1 = _y1;
-		radius = R;
-		height = H;
-		break;
-	default:
-		break;
-	}
+bool Shape::checkEquality(pointCoord _a, pointCoord _b) const {
+	if (_a == _b) {throw std::length_error("Invalid vector length. Must not be zero.");} 
+	else {return false;}
+};
 
-	// считаем площадь фигуры
-	switch (type)
-	{
-	case circle:
-		square = M_PI * R * R;
-		break;
-	case cylinder:
-		square = M_PI * R * R + 2 * R * height;
-		break;
-	default:
-		break;
-	}
+Line::Line(pointCoord _a, pointCoord _b) : Shape (LINE), dot_A(_a), dot_B(_b) {checkEquality(_a, _b);}
+Line::~Line () {}
 
-	// считаем объем фигуры
-	switch (type)
-	{
-	case circle:
-		volume = 0;
-		break;
-	case cylinder:
-		volume = M_PI * R * R * height;
-		break;
-	default:
-		break;
-	}
+Square::Square(pointCoord _a, pointCoord _b) : Shape (SQUARE), dot_A(_a), dot_B(_b) {checkEquality(_a, _b);};
+Square::~Square () {};
+bool Square::checkEquality(pointCoord _a, pointCoord _b) const {
+	if (_a == _b) {throw std::length_error("Invalid vector length. Must not be zero.");}
+	else if (_a || _b) {throw std::length_error("Vertexes collision. Vertexes must form 2D figure.");}
+	else {return false;}
+};
 
-}
+Circle::Circle(pointCoord _a, double _radius) : Shape (CIRLCE), dot_A(_a), radius(_radius) {
+	if (_radius <= 0) throw std::length_error("Invalid vector length. Radius must be more than zero.");
+};
+Circle::~Circle () {};
+
+
+Cube::Cube(pointCoord _a, pointCoord _b, pointCoord _c) : Shape (CUBE), dot_A(_a), dot_B(_b), dot_C(_c) {checkEquality(_a, _b, _c);};
+Cube::~Cube () {};
+bool Cube::checkEquality(pointCoord _a, pointCoord _b, pointCoord _c) const {
+	if (((_a || _b) && (_b || _c)) || ((_a || _b) && (_a || _c))) {throw ("Vertexes collision. Vertexes must form 3D figure.");}
+	else {return false;}
+};
+
+Cylinder::Cylinder(pointCoord _a, double _radius, pointCoord _h) : Shape (CYLINDER), dot_A(_a), radius(_radius), dot_H(_h)  {
+	if (_radius <= 0) throw std::length_error("Invalid vector length. Radius must be more than zero.");
+	checkEquality(_a, _h);
+};
+Cylinder::~Cylinder () {};
--- 7_map-homework/hw.04/shape.h
+++ 7_map-homework/hw.04/shape.h
@@ -1,29 +1,88 @@
 #pragma once
+#include <stdexcept>
 #include<string>
-class Shape
-{
-public:
-	static const int line = 0;
-	static const int sqr = 1;
-	static const int cube = 2;
-	static const int circle = 3;
-	static const int cylinder = 4;
-	Shape() = default;
-	Shape(int type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8);
-	Shape(int type, int _x1, int _y1, double R, double H);
-	int getType() { return type; }
 
-	int type;
-	int x1 = 0, y1 = 0, z1 = 0, 
-		x2 = 0, y2 = 0, z2 = 0, 
-		x3 = 0, y3 = 0, z3 = 0, 
-		x4 = 0, y4 = 0, z4 = 0, 
-		x5 = 0, y5 = 0, z5 = 0, 
-		x6 = 0, y6 = 0, z6 = 0, 
-		x7 = 0, y7 = 0, z7 = 0, 
-		x8 = 0, y8 = 0, z8 = 0;
-	double volume;
-	double square;
-	double height;
+enum Type {
+	LINE,
+	SQUARE,
+	CIRLCE,
+	CUBE,
+	CYLINDER
+};
+
+struct pointCoord {
+	double p_x{};
+	double p_y{};
+	double p_z{};
+	bool operator==(const pointCoord &_a) const {
+		if (p_x == _a.p_x && p_y == _a.p_y && p_z == _a.p_z) return true;	
+	}; 
+	bool operator||(const pointCoord &_a) const {
+		if 		(p_x == _a.p_x && p_y == _a.p_y) {return true;}
+		else if (p_y == _a.p_y && p_z == _a.p_z) {return true;}	
+		else if (p_x == _a.p_x && p_z == _a.p_z) {return true;}
+	}; 
+};
+
+class Shape {
+public:
+	Shape(Type extType);
+	~Shape();
+	double virtual calcLength(pointCoord point_one, pointCoord point_two) const;
+	bool virtual checkEquality(pointCoord _a, pointCoord _b) const;
+	int getType() {
+		return type; 
+	}
+protected:
+	std::string shape_name;
+	Type type;
+};
+
+class Line : public Shape {
+public:
+	Line(pointCoord _a, pointCoord _b) : Shape (LINE), dot_A(_a), dot_B(_b) {};
+	~Line () {};
+private:
+	pointCoord dot_A;
+	pointCoord dot_B;
+};
+
+class Square : public Shape {
+public:
+	Square(pointCoord _a, pointCoord _b) : Shape (SQUARE), dot_A(_a), dot_B(_b) {};
+	~Square () {};
+	bool virtual checkEquality(pointCoord _a, pointCoord _b) const override;
+private:
+	pointCoord dot_A;
+	pointCoord dot_B;
+};
+
+class Circle : public Shape {
+public:
+	Circle(pointCoord _a, double _radius) : Shape (CIRLCE), dot_A(_a), radius(_radius) {};
+	~Circle () {};
+private:
+	pointCoord dot_A;
 	double radius;
+};
+
+class Cube : public Shape {
+public:
+	Cube(pointCoord _a, pointCoord _b, pointCoord _c) : Shape (CUBE), dot_A(_a), dot_B(_b), dot_C(_c) {};
+	~Cube () {};
+	bool checkEquality(pointCoord _a, pointCoord _b, pointCoord _c) const;
+private:
+	pointCoord dot_A;
+	pointCoord dot_B;
+	pointCoord dot_C;
+};
+
+class Cylinder : public Shape {
+public:
+	Cylinder(pointCoord _a, double _radius, pointCoord _h) : Shape (CYLINDER), dot_A(_a), radius(_radius), dot_H(_h) {};
+	~Cylinder () {};
+private:
+	pointCoord dot_A;
+	double radius;
+	pointCoord dot_H;
 };
