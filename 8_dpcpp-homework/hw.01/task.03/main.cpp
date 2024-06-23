76126c7d38e9377058674c78d73441245446d60d
ZuMi <zubetzmikhael@gmail.com>
2024-06-23 20:05:42 +0300

    add 08_hw.01.03 task solution

---
+++ 8_dpcpp-homework/hw.01/task.03/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cassert>
+#include "sql_query_builder.h"
+
+int main () {
+    AdvQueryBuilder query_builder3;
+    std::vector<std::string> select_columns {"name", "phone"};
+    query_builder3.AddColumn(select_columns);
+    
+    query_builder3.AddFrom("students");
+
+    std::map<std::string,std::string> where_vals {{"id", "42"}};
+    query_builder3.AddWhere(where_vals);
+
+    assert((query_builder3.BuildQuery(MORE), "SELECT name, phone FROM students WEHRE id > 42;"));
+    std::cout << "Checkpoint\n";
+
+    return 0;
+}   
---
+++ 8_dpcpp-homework/hw.01/task.03/sql_query_builder.h
@@ -0,0 +1,133 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <stdexcept>
+
+enum WhereTypeLogic {
+    EQUAL,
+    LESS,
+    MORE
+};
+
+struct SqlSelectQuery {
+    std::vector<std::string> select_columns{};
+    std::string from_columns{};
+    std::vector<std::pair<std::string, std::string>> where_requst_vals{};     
+};
+
+
+class SqlSelectQueryBuilder {
+protected:
+    SqlSelectQuery query;
+    std::string select_request{};
+
+public:
+    SqlSelectQueryBuilder() {}
+    ~SqlSelectQueryBuilder() {}
+
+    SqlSelectQueryBuilder& AddColumn(const std::string column) noexcept {
+        this->query.select_columns.push_back(column);
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddColumn(const std::vector<std::string>& columns) noexcept {
+        for (auto& i: columns) {
+            this->query.select_columns.push_back(i);
+        }        
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddFrom(const std::string from_val) noexcept {
+        if (!this->query.from_columns.empty()) {
+            this->query.from_columns.clear();
+        }
+        this->query.from_columns.append(from_val);
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddWhere(const std::string key, const std::string value) noexcept {
+        this->query.where_requst_vals.push_back(std::make_pair(key,value));
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddWhere(const std::map<std::string, std::string>& kv) noexcept {
+        for (const auto& [key, value] : kv) {
+            this->query.where_requst_vals.push_back(std::make_pair(key,value));
+        } 
+        return *this;
+    }
+
+    virtual std::string BuildQuery() {
+        if (this->query.from_columns.empty()) throw std::invalid_argument ("FROM field empty");
+        if (this->query.where_requst_vals.empty()) throw std::invalid_argument ("WHERE fields empty");
+        std::string select_request{};
+        select_request.append("SELECT ");
+        if (this->query.select_columns.empty()) {select_request.append("* ");}
+        else {
+            auto ended = this->query.select_columns.end() - 1;
+            for (auto it = this->query.select_columns.begin(); it != this->query.select_columns.end(); it++) {
+                select_request.append(*it);
+                it != ended ? select_request.append(", ") : select_request.append(" ");
+            }
+        }
+
+        select_request.append("FROM ").append(this->query.from_columns).append(" WEHRE");
+        {
+            auto it = this->query.where_requst_vals.begin(); auto ended = this->query.where_requst_vals.end() - 1;
+            for (auto smt: this->query.where_requst_vals) {
+                select_request.append(" ").append(smt.first).append(" = ").append(smt.second);
+                if (it != ended) {select_request.append(" AND");}
+                it++;
+            }
+        }    
+        select_request.append(";");
+        return select_request;
+    }
+
+};
+
+
+class AdvQueryBuilder : public SqlSelectQueryBuilder {
+private:
+
+public:
+    AdvQueryBuilder() {}
+    ~AdvQueryBuilder() {}
+
+    std::string BuildQuery(WhereTypeLogic type) {
+        if (this->query.from_columns.empty()) throw std::invalid_argument ("FROM field empty");
+        if (this->query.where_requst_vals.empty()) throw std::invalid_argument ("WHERE fields empty");
+        
+        std::string logic;
+        switch (type) {
+            case EQUAL: logic = " = "; break;
+            case LESS: logic = " < "; break;
+            case MORE: logic = " > "; break;        
+            default: throw std::invalid_argument ("Invalid logic operator in WHERE field"); break;
+        }
+
+        std::string select_request{};
+        select_request.append("SELECT ");
+        if (this->query.select_columns.empty()) {select_request.append("* ");}
+        else {
+            auto ended = this->query.select_columns.end() - 1;
+            for (auto it = this->query.select_columns.begin(); it != this->query.select_columns.end(); it++) {
+                select_request.append(*it);
+                it != ended ? select_request.append(", ") : select_request.append(" ");
+            }
+        }
+
+        select_request.append("FROM ").append(this->query.from_columns).append(" WEHRE");
+        {
+            auto it = this->query.where_requst_vals.begin(); auto ended = this->query.where_requst_vals.end() - 1;
+            for (auto smt: this->query.where_requst_vals) {
+                select_request.append(" ").append(smt.first).append(logic).append(smt.second);
+                if (it != ended) {select_request.append(" AND");}
+                it++;
+            }
+        }    
+        select_request.append(";");
+        return select_request;
+    }
+
+};
