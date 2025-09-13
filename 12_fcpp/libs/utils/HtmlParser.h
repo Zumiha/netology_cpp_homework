1a2458538b010b3d51335e20203b9932c4ac9f76
Zub Mik <zubetzmikhael@gmail.com>
2025-09-13 16:16:30 +0300

    cmake C++20

--- 12_fcpp/libs/utils/HtmlParser.h
+++ 12_fcpp/libs/utils/HtmlParser.h
@@ -17,12 +17,12 @@ struct ParsedContent {
 
 class HtmlParser {
 private:
-    static std::string stripHtmlTags(const std::string& html);
+    static std::string decodeHtmlEntities(const std::string& text);
     static std::string normalizeText(const std::string& text);
 
-    static std::string HtmlParser::getTitle(const std::string& html);
+    static std::string extractBodyText(const std::string& html);
     static std::vector<std::string> extractLinks(const std::string& html);
-    static std::string decodeHtmlEntities(const std::string& text);
+    static std::string HtmlParser::getTitle(const std::string& html);
 public:
     static std::optional<ParsedContent> processHtml(const std::optional<std::string>& html_content, const Link& base_link, int search_depth = 1);
     static std::vector<UrlInfo> resolveUrls(const std::vector<std::string>& links, const Link& base_link, int search_depth = 1);
