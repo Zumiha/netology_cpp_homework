7595cbbe3d1fbb4dcd5df612db01931b2297dd0c
ZuMi <zubetzmikhael@gmail.com>
2025-09-27 01:00:43 +0300

    changed project structure to build different execution files for WebCrawler and SearchEngine(not yet implemented)

---
+++ 12_fcpp/src/crawler/main_crawl.cpp
@@ -0,0 +1,17 @@
+#include "Crawler.h"
+
+int main(int argc, char* argv[]) {   
+    try {
+        webCrawler spider(argc, argv);
+        // spider.printSettings();  
+        std::cout << "Creating crawler";
+        spider.startCrawling();
+        std::cout << "\n...Crawler destroyed....\n";
+
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+    return 0;
+}
